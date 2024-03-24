04f9ebf354be6ca7cb1be6328f71d7d202d320d2
xrmaalk <xinra.inc@gmail.com>
2024-03-24 14:35:36 -0600

    Setup dotenv via npm i dotenv

--- server.mjs
+++ server.mjs
@@ -1,5 +1,9 @@
 import express from "express";
-// import * as UserData from "./users.js";
+import { config as dotenvConfig } from "dotenv"; // Setup npm i dotenv for use in app
+import * as UserData from "./api/users.mjs"; // Test route using user data
+//import * as UserData from "./users.js";
+
+dotenvConfig();
 
 const app = express();
 
@@ -8,17 +12,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/users", (req, res) => {
+app.get("./api/users", (req, res) => {
   const users = UserData.getAllUsers();
   res.send(users);
 });
 
-app.get("/users/:name", (req, res) => {
+app.get("./api/users/:name", (req, res) => {
   const record = UserData.getUser(req.params.name);
   res.send(record);
 });
 
-app.delete("/users/:name", (req, res) => {
+app.delete("./api/users/:name", (req, res) => {
   const name = req.params.name;
   UserData.delete(name);
   res.status(200).send("ok");
