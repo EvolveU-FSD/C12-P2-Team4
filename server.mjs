7b28eca20c64a5f24da2fc4673b2e50847b34c61
xrmaalk <xinra.inc@gmail.com>
2024-03-24 15:03:20 -0600

    solve cannot get error

--- server.mjs
+++ server.mjs
@@ -1,28 +1,28 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { config as dotenvConfig } from "dotenv"; // Setup npm i dotenv for use in app
-import * as UserData from "./api/users.mjs"; // Test route using user data
+import * as UserData from "./public/api/users.mjs"; // Test route using user data
 //import * as UserData from "./users.js";
 
 dotenvConfig();
-
-const app = express();
-
 const PORT = process.env.PORT || 3000;
-
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("./api/users", (req, res) => {
+app.get("/api/users", (req, res) => {
   const users = UserData.getAllUsers();
   res.send(users);
 });
 
-app.get("./api/users/:name", (req, res) => {
+app.get("/api/users/:name", (req, res) => {
   const record = UserData.getUser(req.params.name);
   res.send(record);
 });
 
-app.delete("./api/users/:name", (req, res) => {
+app.delete("/api/users/:name", (req, res) => {
   const name = req.params.name;
   UserData.delete(name);
   res.status(200).send("ok");
