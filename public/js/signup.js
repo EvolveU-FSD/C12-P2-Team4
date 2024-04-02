0a92527f9724034bf2529f6689381c7504b5447d
xrmaalk <xinra.inc@gmail.com>
2024-04-02 15:49:25 -0600

    local repo cleanup

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,34 +2,34 @@
 document
   .getElementById("openSignUpModal")
   .addEventListener("click", function () {
-    document.getElementById("signUpModal").style.display = "flex";
-  });
+    document.getElementById("signUpModal").style.display = "flex"
+  })
 
 document.querySelectorAll(".close").forEach(function (closeButton) {
   closeButton.addEventListener("click", function () {
-    this.closest(".modal").style.display = "none";
-  });
-});
+    this.closest(".modal").style.display = "none"
+  })
+})
 
 window.addEventListener("click", function (event) {
   if (event.target.classList.contains("modal")) {
-    event.target.style.display = "none";
+    event.target.style.display = "none"
   }
-});
+})
 
 //-------------- FORM HANDLING -------------------//
 document
   .getElementById("signUpForm")
   .addEventListener("submit", async function (event) {
-    event.preventDefault(); // Prevent default form submission behavior
+    event.preventDefault() // Prevent default form submission behavior
 
     try {
-      const formData = new FormData(this); // Get form data
+      const formData = new FormData(this) // Get form data
 
       // Construct an object from FormData
-      const formDataObject = {};
+      const formDataObject = {}
       for (const [key, value] of formData.entries()) {
-        formDataObject[key] = value;
+        formDataObject[key] = value
       }
 
       //---------- COLLECT RESPONSE --------------//
@@ -39,17 +39,17 @@ document
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formDataObject), // Convert FormData object to JSON
-      });
+      })
       //---------- CHECK FOR SUCCESS --------//
       if (response.ok) {
-        const data = await response.json();
-        alert(data.message); // Display success message
+        const data = await response.json()
+        alert(data.message) // Display success message
       } else {
-        const errorData = await response.json();
-        alert(errorData.error); // Display error message
+        const errorData = await response.json()
+        alert(errorData.error) // Display error message
       }
     } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred. Please try again.", error); // Display error message
+      console.error("Error:", error)
+      alert("An error occurred. Please try again.", error) // Display error message
     }
-  });
+  })
--- server.mjs
+++ server.mjs
@@ -1,134 +1,134 @@
 //----- IMPORT STATEMENTS ---------//
-import axios from "axios";
-import express from "express";
-import path from "path";
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import { fileURLToPath } from "url";
-import { config as dotenvConfig } from "dotenv";
-import User from "./database/mongodb-mongoose/model/userOperations.js";
+import axios from "axios"
+import express from "express"
+import path from "path"
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import { fileURLToPath } from "url"
+import { config as dotenvConfig } from "dotenv"
+import User from "./database/mongodb-mongoose/model/userOperations.js"
 
 //--------------- FUNCTION CALLS ----------------//
-dotenvConfig();
+dotenvConfig()
 
 //---------------- VARIABLES ---------------------//
-const PORT = process.env.PORT || 3000;
-const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const uri = process.env.MONGODB;
-const apiKey = process.env.GOOGLEMAPS_API_KEY;
+const PORT = process.env.PORT || 3000
+const app = express()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const uri = process.env.MONGODB
+const apiKey = process.env.GOOGLEMAPS_API_KEY
 //-------------- MONGOOSE CONNECTION ------------//
 mongoose
   .connect(uri)
   .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
 //-------------- SERVER FUNCTIONS ----------------//
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public"))); // used to convert filepath to url
-app.use(express.static(path.join(__dirname, "googleMaps")));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public"))) // used to convert filepath to url
+app.use(express.static(path.join(__dirname, "googleMaps")))
 
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", function () {
-  console.log("Connected to MongoDB");
-});
+  console.log("Connected to MongoDB")
+})
 
-const publicArtSchema = new mongoose.Schema({}, { collection: "public-art" });
-const PublicArt = mongoose.model("PublicArt", publicArtSchema);
+const publicArtSchema = new mongoose.Schema({}, { collection: "public-art" })
+const PublicArt = mongoose.model("PublicArt", publicArtSchema)
 
 //---------------- GET API HANDLES ------------------//
 app.get("/api/public-art", async (req, res) => {
-  const data = await PublicArt.find({}).sort({ title: 1 });
-  res.json(data);
-});
+  const data = await PublicArt.find({}).sort({ title: 1 })
+  res.json(data)
+})
 
 app.get("/places", async (req, res) => {
   try {
-    const { query } = req.query;
+    const { query } = req.query
     const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(
       query
-    )}&key=${apiKey}`;
+    )}&key=${apiKey}`
 
-    const response = await axios.get(url);
-    res.json(response.data);
+    const response = await axios.get(url)
+    res.json(response.data)
   } catch (error) {
-    res.status(500).json({ error: error.toString() });
+    res.status(500).json({ error: error.toString() })
   }
-});
+})
 
 // Serve .mjs files with the correct MIME type
 app.get("*.mjs", (req, res, next) => {
-  res.type("application/javascript");
-  next();
-});
+  res.type("application/javascript")
+  next()
+})
 
 // Serve CSS files with the correct MIME type
 app.get("*.css", (req, res, next) => {
-  res.type("text/css");
-  next();
-});
+  res.type("text/css")
+  next()
+})
 
 // Serve map.html file
 app.get("/maps", (req, res) => {
-  res.sendFile(path.join(__dirname, "googleMaps", "map.html"));
-});
+  res.sendFile(path.join(__dirname, "googleMaps", "map.html"))
+})
 
 // API routes
 app.get("/api/users", (req, res) => {
-  const users = UserData.getAllUsers();
-  res.send(users);
-});
+  const users = UserData.getAllUsers()
+  res.send(users)
+})
 
 app.get("/api/users/:name", (req, res) => {
-  const record = UserData.getUser(req.params.name);
-  res.send(record);
-});
+  const record = UserData.getUser(req.params.name)
+  res.send(record)
+})
 
 //----------------- POST API ROUTE --------------//
 //            SIGNIN HANDLING                    //
 app.post("/signin", async (req, res) => {
   // Change route to POST
   try {
-    let user = await User.findOne({ username: req.body.username });
+    let user = await User.findOne({ username: req.body.username })
 
     if (!user) {
-      return res.status(400).json({ error: "Invalid credentials try again" });
+      return res.status(400).json({ error: "Invalid credentials try again" })
     }
     const passwordCompare = await bcrypt.compare(
       req.body.password,
       user.password
-    );
+    )
     if (!passwordCompare) {
       return res
         .status(400)
-        .join({ error: "Enter valid credntials to continue." });
+        .join({ error: "Enter valid credntials to continue." })
     }
 
-    res.json({ sucess: "Authenication Accepted" });
-    console.log(`Logged in as ${user}`);
+    res.json({ sucess: "Authenication Accepted" })
+    console.log(`Logged in as ${user}`)
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    console.error(error)
+    res.status(500).json({ error: "Internal server error" })
   }
-});
+})
 
 //               SIGNP HANDLING                  //
 app.post("/signup", async (req, res) => {
-  const salt = await bcrypt.genSalt(10);
-  const secPass = await bcrypt.hash(req.body.password, salt);
-  console.log(secPass);
+  const salt = await bcrypt.genSalt(10)
+  const secPass = await bcrypt.hash(req.body.password, salt)
+  console.log(secPass)
 
   try {
-    const { firstname, lastname, username, email, password } = req.body;
+    const { firstname, lastname, username, email, password } = req.body
 
     // Check if the user already exists
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email })
     if (existingUser) {
       return res
         .status(400)
-        .json({ error: "User already exists, Please Signin" });
+        .json({ error: "User already exists, Please Signin" })
     }
 
     // Create a new user
@@ -138,28 +138,28 @@ app.post("/signup", async (req, res) => {
       username,
       email,
       password: secPass,
-    });
-    await newUser.save();
+    })
+    await newUser.save()
 
     // Return success response
 
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({ message: "User created successfully" })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    console.error(error)
+    res.status(500).json({ error: "Internal server error" })
   }
-});
+})
 
 //------------- DELETE API ROUTE ---------------//
 app.delete("/api/users/:name", (req, res) => {
-  const name = req.params.name;
-  UserData.delete(name);
-  res.status(200).send("ok");
-});
+  const name = req.params.name
+  UserData.delete(name)
+  res.status(200).send("ok")
+})
 
 app.listen(PORT, () => {
-  console.log(`Running on http://localhost:${PORT}`);
-});
+  console.log(`Running on http://localhost:${PORT}`)
+})
 
 //---------- DISCONNECT FROM DATABASE ----------//
 //           mongoose.disconnect();            //
