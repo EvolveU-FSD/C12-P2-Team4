9ce441daa0802fefbb3b048de1b2d691dd25d2c1
Shana Adams <shana.adams10@gmail.com>
2024-04-09 17:14:12 -0600

    add jwt session code to 'controllers' folder.

--- server/controllers/jsonwebtoken.js
+++ server/controllers/jsonwebtoken.js
@@ -1,7 +1,49 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+import dotenv from "dotenv";
+dotenv.config();
+import React, { useState } from "react";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 app.use(express.json());
+
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
+
+app.post("/users/login", async (req, res) => {
+  //Authenticate user
+  const user = users.find((user) => (user.name = req.body.name));
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+  res.json({ accessToken: accessToken });
+});
+//middleware to authenticate token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err)
+      return sendStatus(403).send("You no longer have access, Please sign in");
+    req.user = user;
+    next();
+  });
+}
