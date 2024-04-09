29171191bb42e3a72de03c34004c4cac8bb7b3a0
xrmaalk <xinra.inc@gmail.com>
2024-04-09 06:03:39 -0600

    create ItineraryCard jsx

--- client/src/components/ItineraryCard/ItineraryCard.jsx
+++ client/src/components/ItineraryCard/ItineraryCard.jsx
@@ -1,3 +1,14 @@
+import userModel from "../../../../server/models/userModel"
+import "./ItineraryCard.css"
+import artPic from "../../assets/images/publicart/bears.jpg"
+
 export default function ItineraryCard() {
-  return <div>ItineraryCard</div>
+  return (
+    <div className="card">
+      <img src={artPic} alt="Place to see"></img>
+      <h2>Day: {userModel.firstname}</h2>
+      <h3>User: {userModel.firstname}</h3>
+      <p>Place to visit</p>
+    </div>
+  )
 }
