2af1db6c46deb2b3ccbadcba6b5ccc1605cf8b37
xrmaalk <xinra.inc@gmail.com>
2024-04-10 01:17:29 -0600

    revert back to Calendar from Calendar

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -10,8 +10,8 @@ import {
   UsersIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline"
-// import Calendar from "../../Pages/Calendar/Calendar"
-import Calendar from "../../Pages/Calendar/Day"
+import Calendar from "../../Pages/Calendar/Calendar"
+// import Calendar from "../../Pages/Calendar/Day"
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
