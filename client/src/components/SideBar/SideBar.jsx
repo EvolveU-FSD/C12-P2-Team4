77ef455846534973caaa95110d859f4dc6898049
xrmaalk <xinra.inc@gmail.com>
2024-04-09 23:18:58 -0600

    add reset css to index css

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -292,10 +292,10 @@ export default function SideBar() {
 
         <main className="lg:pl-72">
           <div className="xl:pl-96">
+            <Calendar />
             <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
               {/* Main area */}
             </div>
-            <Calendar />
           </div>
         </main>
 
