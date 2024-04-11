62284b438a897196905fd1113ba7368b6e06888e
Shana Adams <shana.adams10@gmail.com>
2024-04-11 17:30:46 -0600

    add NavBar to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 // App.jsx
-import React from "react"
-import SignIn from "./components/Signin/SignIn"
-import ItineraryCard from "./components/ItineraryCard/ItineraryCard"
-import SideBar from "./components/SideBar/SideBar"
-import CalgaryMap from "./components/MapComponents/Map"
-import Footer from "./components/Footer/Footer"
-import SearchBar from "./components/ReusableComponents/SearchBar"
-import SignUp from "./components/SignUp/SignUp"
-import "./index.css"
+import React from "react";
+import SignIn from "./components/Signin/SignIn";
+import ItineraryCard from "./components/ItineraryCard/ItineraryCard";
+import SideBar from "./components/SideBar/SideBar";
+import CalgaryMap from "./components/MapComponents/Map";
+import Footer from "./components/Footer/Footer";
+import SearchBar from "./components/ReusableComponents/SearchBar";
+import SignUp from "./components/SignUp/SignUp";
+import NavBar from "./components/ReusableComponents/NavBar";
+import "./index.css";
 
 const App = () => {
   return (
     <>
-      <div className="signin">
-        {/* <SideBar /> */}
-        <SignIn />
-        <SignUp />
+      <div className="navbar">
+        <NavBar />
+        <div className="signin">
+          {/* <SideBar /> */}
+          <SignIn />
+          <SignUp />
+        </div>
       </div>
       <div className="map-container">
         <div className="itinerary">
@@ -32,7 +36,7 @@ const App = () => {
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
