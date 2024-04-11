51ebb2d7789693183448395f070bb31295ef33a7
Shana Adams <shana.adams10@gmail.com>
2024-04-11 15:51:22 -0600

    Remove previous styling from SearchBar.jsx

--- client/src/components/ReusableComponents/SearchBar.jsx
+++ client/src/components/ReusableComponents/SearchBar.jsx
@@ -14,16 +14,5 @@ function SearchBar() {
     </div>
   );
 }
-//     <div className="flex justify-center space-x-4">
-//       <button className="px-4 py-2 text-white bg-blue-500 rounded">
-//         <SearchIcon className="w-5 h-5 mr-2" />
-//         Search
-//       </button>
-//       <input
-//         className="w-48 p-2 border rounded border-gray-300"
-//         type="text"
-//         placeholder=" restaurants, hotel, bar etc."
-//       />
-//     </div>
-//   );
+
 export default SearchBar;
