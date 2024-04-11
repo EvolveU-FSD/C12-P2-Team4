0e49ef15c2bd6268ca8140a3c8f96a2990bfb6c8
Shana Adams <shana.adams10@gmail.com>
2024-04-11 17:30:29 -0600

    add styling to NavBar.jsx

--- client/src/components/ReusableComponents/NavBar.jsx
+++ client/src/components/ReusableComponents/NavBar.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 function NavBar() {
-  return <></>;
+  return (
+    <nav className="bg-white text-black p-4">
+      <div className="container mx-auto flex justify-between">
+        <div className="font-bold">Logo</div>
+        <div></div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
