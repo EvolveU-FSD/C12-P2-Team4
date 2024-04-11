6c58b308712d78686198ff0f152f1c7893251195
Shana Adams <shana.adams10@gmail.com>
2024-04-11 15:42:00 -0600

    add searchbar icon.

--- client/src/components/ReusableComponents/SearchBar.jsx
+++ client/src/components/ReusableComponents/SearchBar.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
-
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 function SearchBar() {
   return (
-    <div className="search-bar">
-      <button>Search</button>
-      <input type="text" placeholder="Search..." />
+    <div className="flex justify-center space-x-4">
+      <input
+        className="w-48 p-2 border rounded border-gray-300"
+        type="text"
+        placeholder="Search..."
+      />
+      <button className="flex items-center px-4 py-2 text-white bg-blue-500 rounded">
+        <MagnifyingGlassIcon className="h-5 w-5" />
+      </button>
     </div>
   );
 }
-
+//     <div className="flex justify-center space-x-4">
+//       <button className="px-4 py-2 text-white bg-blue-500 rounded">
+//         <SearchIcon className="w-5 h-5 mr-2" />
+//         Search
+//       </button>
+//       <input
+//         className="w-48 p-2 border rounded border-gray-300"
+//         type="text"
+//         placeholder=" restaurants, hotel, bar etc."
+//       />
+//     </div>
+//   );
 export default SearchBar;
