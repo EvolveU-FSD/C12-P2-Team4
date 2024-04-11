f627a09e82b94ce187c5af540d852fe6d393cf46
Shana Adams <shana.adams10@gmail.com>
2024-04-11 15:50:43 -0600

    Add colors to search bar.

--- client/src/components/ReusableComponents/SearchBar.jsx
+++ client/src/components/ReusableComponents/SearchBar.jsx
@@ -3,14 +3,14 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 function SearchBar() {
   return (
     <div className="flex justify-center space-x-4">
+      <button className="flex items-center px-4 py-2 text-white bg-black rounded">
+        <MagnifyingGlassIcon className="h-5 w-5" />
+      </button>
       <input
-        className="w-48 p-2 border rounded border-gray-300"
+        className="w-48 p-2 border rounded bg-[#F2EEED] border-gray-300"
         type="text"
         placeholder="Search..."
       />
-      <button className="flex items-center px-4 py-2 text-white bg-blue-500 rounded">
-        <MagnifyingGlassIcon className="h-5 w-5" />
-      </button>
     </div>
   );
 }
