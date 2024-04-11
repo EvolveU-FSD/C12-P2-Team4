664f10c045c699c2b847351b3700271680c22461
Shana Adams <shana.adams10@gmail.com>
2024-04-11 11:28:06 -0600

    map api after google install

--- client/src/components/MapComponents/map.jsx
+++ client/src/components/MapComponents/map.jsx
@@ -1,49 +1,51 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import "./map.css"
+import React from "react";
+import { GoogleMap, useJsApiLoader } from " @react-google-maps/api";
+import "./map.css";
 
 const containerStyle = {
-    width: '400px',
-    height: '400px'
+  width: "400px",
+  height: "400px",
 };
 
 const center = {
-    lat: 51.0447,
-    lng: -114.0719
+  lat: 51.0447,
+  lng: -114.0719,
 };
 
 function CalgaryMap() {
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: ""
-    })
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "",
+  });
 
-    const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
 
-    const onLoad = React.useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
 
-        setMap(map)
-    }, [])
+    setMap(map);
+  }, []);
 
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
-    return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={8}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-        >
-            { /* Child components, such as markers, info windows, etc. */}
-            <></>
-        </GoogleMap>
-    ) : <></>
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={8}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
-export default CalgaryMap
+export default CalgaryMap;
