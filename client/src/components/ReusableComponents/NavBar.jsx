30efa156e7e0d2b74b1f1bb5de95a838657e6720
Shana Adams <shana.adams10@gmail.com>
2024-04-12 08:59:09 -0600

    Add useState to show signin/signup on the navbar when user is not logged in & profile icon when the user is logged in.

--- client/src/components/ReusableComponents/NavBar.jsx
+++ client/src/components/ReusableComponents/NavBar.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-
+import SignIn from "../../SignIn/SignIn";
+import SignUp from "SignUp";
+import Profile from "Profile";
 function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <nav className="bg-white text-black p-4">
       <div className="container mx-auto flex justify-between">
-        <div className="font-bold">Logo</div>
-        <div></div>
+        {/* <div className="font-bold">Logo</div> */}
+        <div className="flex space-x-4">
+          {isLoggedIn ? (
+            <Profile />
+          ) : (
+            <>
+              <SignIn onLogin={() => setIsLoggedIn(true)} />
+              <SignUp />
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
