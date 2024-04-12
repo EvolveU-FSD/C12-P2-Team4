9e9b71c5308f46dc766cb05d19c97dda15975652
Shana Adams <shana.adams10@gmail.com>
2024-04-12 14:31:14 -0600

    Add delete icon to Delete button in Delete.jsx

--- client/src/components/ReusableComponents/Delete.jsx
+++ client/src/components/ReusableComponents/Delete.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Icon } from "@heroicons/react/24/solid";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 //LargeButton
 
@@ -7,7 +7,7 @@ function DeleteButton() {
   return (
     <div className="flex justify-center space-x-4">
       <button className="flex items-center px-4 py-2 text-black bg-white rounded hover:bg-[#F2EEED] active:bg-[#F2EEED]">
-        {/* <MagnifyingGlassIcon className="h-5 w-5" /> */}
+        <TrashIcon className="h-5 w-5" />
         <span className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity">
           DELETE
         </span>
