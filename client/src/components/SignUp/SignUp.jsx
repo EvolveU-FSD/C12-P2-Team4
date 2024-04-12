0c1e89a6f6d43d1ca15f1bde7a70fe9cc2a643c8
xrmaalk <xinra.inc@gmail.com>
2024-04-12 11:17:31 -0600

    add SignUp.jsx logic

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react"
+import { useEffect } from "react"
 
 import "../Signin/signin.css"
 
 export default function SignUp() {
   const [modalOpen, setModalOpen] = useState(false)
+  const [loadError, setLoadError] = useState()
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
   const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
   const openModal = () => setModalOpen(true)
@@ -13,13 +18,20 @@ export default function SignUp() {
   const handleSignUp = async (event) => {
     event.preventDefault() // Prevent the default form submission
 
+    console.log(event)
     try {
       const response = await fetch("/api/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password }), // Send username and password as JSON in the request body
+        body: JSON.stringify({
+          firstname,
+          lastname,
+          username,
+          email,
+          password,
+        }), // Send username and password as JSON in the request body
       })
 
       if (response.ok) {
@@ -31,11 +43,15 @@ export default function SignUp() {
         alert(errorData.error) // Display error message from response
       }
     } catch (error) {
+      setLoadError(error)
       console.error("Error:", error)
       alert("An error occurred. Please try again.") // Display error message
     }
   }
 
+  const handleChange = (setter) => (event) => {
+    setter(event.target.value)
+  }
   return (
     <>
       <a onClick={openModal} id="openSignUpModal" className="openModal">
@@ -58,6 +74,7 @@ export default function SignUp() {
                 required
                 minLength="2"
                 maxLength="20"
+                onChange={handleChange(setFirstname)}
               />
               <label htmlFor="lastname">Last Name:</label>
               <input
@@ -67,6 +84,7 @@ export default function SignUp() {
                 required
                 minLength="2"
                 maxLength="20"
+                onChange={handleChange(setLastname)}
               />
               <label htmlFor="signUpUsername">Username:</label>
               <input
@@ -76,6 +94,7 @@ export default function SignUp() {
                 required
                 minLength="6"
                 maxLength="32"
+                onChange={handleChange(setUsername)}
               />
               <label htmlFor="email">Email:</label>
               <input
@@ -85,6 +104,7 @@ export default function SignUp() {
                 required
                 minLength="6"
                 maxLength="32"
+                onChange={handleChange(setEmail)}
               />
               <label htmlFor="signUpPassword">Password:</label>
               <input
@@ -94,10 +114,9 @@ export default function SignUp() {
                 required
                 minLength="9"
                 maxLength="64"
+                onChange={handleChange(setPassword)}
               />
-              <button onClick={handleSignUp} type="submit">
-                Sign Up
-              </button>
+              <button type="submit">Sign Up</button>
             </form>
           </div>
         </div>
