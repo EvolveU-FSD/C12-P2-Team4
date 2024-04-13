01b12e2a10e77eea191c3995b2095f5b580fe32a
Shana Adams <shana.adams10@gmail.com>
2024-04-12 19:45:21 -0600

    Import icons from the icon library for SaveButtons.

--- client/src/components/ReusableComponents/Save.jsx
+++ client/src/components/ReusableComponents/Save.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import { TrashIcon } from "@heroicons/react/24/solid";
+import { CheckIcon, PlusCircleIcon, CheckCircleIcon, BookmarkIcon } from "@heroicons/react/24/solid";
 
 //PageButton
-
 function SaveButton() {
   return (
     <div className="flex justify-center space-x-4">
       <button className="flex items-center px-4 py-2 text-black bg-white rounded hover:bg-[#F2EEED] active:bg-[#F2EEED]">
-        <TrashIcon className="h-5 w-5" />
+        <Icon className="h-5 w-5" />
         <span className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity">
           Save
         </span>
