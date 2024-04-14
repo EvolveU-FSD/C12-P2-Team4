1060c8f59839e3548b4370d4a5d73a596213c4f4
Shana Adams <shana.adams10@gmail.com>
2024-04-13 19:24:24 -0600

    Adjust margin-left of search icon on search bar to not touch container border.

--- client/src/components/ReusableComponents/SearchBar.jsx
+++ client/src/components/ReusableComponents/SearchBar.jsx
@@ -3,7 +3,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 function SearchBar() {
   return (
     <div className="flex justify-center space-x-4">
-      <button className="flex items-center px-4 py-2 text-white bg-black rounded">
+      <button className="flex items-center px-4 py-2 text-white bg-black rounded  ml-2.5">
         <MagnifyingGlassIcon className="h-5 w-5" />
       </button>
       <input
