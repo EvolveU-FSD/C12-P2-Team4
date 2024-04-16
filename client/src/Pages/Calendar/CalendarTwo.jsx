25bc12dfac7831e8e2bd29c22edc2866eeee2d59
Shana Adams <shana.adams10@gmail.com>
2024-04-16 16:53:59 -0600

    reset calendar page.

--- client/src/Pages/Calendar/CalendarTwo.jsx
+++ client/src/Pages/Calendar/CalendarTwo.jsx
@@ -1,33 +1,7 @@
 import React from "react";
-import { useState } from "react";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-import { format } from "date-fn";
 
 const Calendar = () => {
-  //tracks the selected date
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  //renders the date in the currently visable month.
-  const [activeDate, setActiveDate] = useState(new Date());
-  const getHeader = () => {
-    return (
-      <div className="header">
-        <div className="todayButton">Today</div>
-        <AiOutlineLeft className="navIcon" />
-        <AiOutlineRight className="navIcon" />
-        <h2 className="currentMonth">{format(activeDate, "MMMM yyyy")}</h2>
-      </div>
-    );
-  };
-  const getWeekDaysNames = () => {};
-  const getDates = () => {};
-
-  return (
-    <section>
-      {getHeader()}
-      {getWeekDaysNames()}
-      {getDates()}
-    </section>
-  );
+  return <section>Hello Callendar</section>;
 };
 
 export default Calendar;
