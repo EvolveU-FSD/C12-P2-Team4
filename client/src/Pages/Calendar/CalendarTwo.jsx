a6a8aa27a7ad07d7e853152189c5a4e501ee8dff
Shana Adams <shana.adams10@gmail.com>
2024-04-16 16:59:13 -0600

    add two use states to show the current day/month.

--- client/src/Pages/Calendar/CalendarTwo.jsx
+++ client/src/Pages/Calendar/CalendarTwo.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calendar = () => {
-  return <section>Hello Callendar</section>;
+  //Keeps track of selected date
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  //Renders the dates in the currently visible month
+  const [activeDate, setActiveDate] = useState(new Date());
+  const getHeader = () => {};
+  const getWeekDaysNames = () => {};
+  const getDates = () => {};
+
+  return (
+    <section>
+      {getHeader()}
+      {getWeekDaysNames()}
+      {getDates()}
+    </section>
+  );
 };
 
 export default Calendar;
