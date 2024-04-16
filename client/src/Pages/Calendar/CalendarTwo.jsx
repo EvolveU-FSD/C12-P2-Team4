04e3d902ad9e0658956972fb6baaa69fd8d5ab88
Shana Adams <shana.adams10@gmail.com>
2024-04-16 17:16:46 -0600

    Calendar header in const getHeader is working.

--- client/src/Pages/Calendar/CalendarTwo.jsx
+++ client/src/Pages/Calendar/CalendarTwo.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import { format } from "date-fns";
 
 const Calendar = () => {
   //Keeps track of selected date
   const [selectedDate, setSelectedDate] = useState(new Date());
   //Renders the dates in the currently visible month
   const [activeDate, setActiveDate] = useState(new Date());
-  const getHeader = () => {};
+
+  const getHeader = () => {
+    return (
+      <div class="header">
+        <div className="todayButton">Today</div>
+        <AiOutlineLeft className="navIcon" />
+        <AiOutlineRight className="navIcon" />
+        <h2 className="currentMonth">{format(activeDate, "MMM yyyy")}</h2>
+      </div>
+    );
+  };
   const getWeekDaysNames = () => {};
   const getDates = () => {};
 
