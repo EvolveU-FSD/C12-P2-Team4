652480b34fcaebfceb93845b04d6f0674da46c33
Shana Adams <shana.adams10@gmail.com>
2024-04-16 16:48:48 -0600

    add header to CalendarTwo.jsx

--- client/src/Pages/Calendar/CalendarTwo.jsx
+++ client/src/Pages/Calendar/CalendarTwo.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import { format } from "date-fn";
 
 const Calendar = () => {
-  const getHeader = () => {};
+  //tracks the selected date
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  //renders the date in the currently visable month.
+  const [activeDate, setActiveDate] = useState(new Date());
+  const getHeader = () => {
+    return (
+      <div className="header">
+        <div className="todayButton">Today</div>
+        <AiOutlineLeft className="navIcon" />
+        <AiOutlineRight className="navIcon" />
+        <h2 className="currentMonth">{format(activeDate, "MMMM yyyy")}</h2>
+      </div>
+    );
+  };
   const getWeekDaysNames = () => {};
   const getDates = () => {};
 
