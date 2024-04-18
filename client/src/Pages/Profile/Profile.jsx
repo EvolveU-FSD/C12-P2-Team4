554ecdc25640412d807dd425d7d9dbf09cb1fa47
xrmaalk <xinra.inc@gmail.com>
2024-04-18 12:23:11 -0600

    add profile info fields

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react"
+
 import NavBar from "../../components/ReusableComponents/NavBar"
-import SideBar from "../../components/SideBar/SideBar"
 import Footer from "../../components/Footer/Footer"
 import UserProfile from "../../components/UserProfile/UserProfile"
 import "../../index.css"
@@ -9,12 +9,12 @@ export default function Profile() {
   return (
     <>
       <NavBar />
-      {/* <SideBar /> */}
+
       <section className="profile flex-1 flex-col gap-6 px-5 py-6">
-        <h1>Profile</h1>
         <div>
           <i className="fa-solid fa-pen-to-square"></i>
         </div>
+
         <div>
           <UserProfile />
         </div>
--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState, useContext } from "react"
+import { PaperClipIcon } from "@heroicons/react/20/solid"
 import { getUserById } from "../../api"
 import { useParams } from "react-router-dom"
 import { AuthContext } from "../Auth/AuthProvider"
+
 function UserProfile() {
   const { auth } = useContext(AuthContext)
   console.log("from profile auth:", auth)
@@ -34,10 +36,12 @@ function UserProfile() {
         ) // Log or handle the response data
       } else {
         const errorData = await response.json()
+        setLoadError(errorData)
         alert(errorData.error) // Display error message from response
       }
     } catch (error) {
       console.error("Error:", error)
+      setLoadError(error)
       alert("An error occurred. Please try again.", error) // Display error message
     }
   }
@@ -54,9 +58,63 @@ function UserProfile() {
   return (
     <div>
       {loadError && <div>Error: {loadError.message}</div>}
-      <div>UserId: {user._id}</div>
-      <div>username: {user.username}</div>
-      <div>email: {user.email}</div>
+
+      <div className="text-base font-semibold leading-7 justify-center text-green-300">
+        <div className="px-4 sm:px-0">
+          <h3>{user.username && <span>PROFILE</span>}</h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-200">
+            User Information
+          </p>
+        </div>
+        <div className="font-medium text-indigo-300 hover:text-indigo-500 cursor-pointer">
+          <div>Username: {user.username}</div>
+          <div>Email: {user.email}</div>
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Full name
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span>Place Holder</span>
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Address
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                Place Holder Address
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                Place holder Email
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Bio
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                Place Holder Bio
+              </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Password
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                Change Password
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
     </div>
   )
 }
