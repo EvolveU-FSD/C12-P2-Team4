7c0ccc808e6eb8b04a1849f3904f07f755652375
xrmaalk <xinra.inc@gmail.com>
2024-04-18 22:04:43 -0600

    adjust modal location

--- client/src/components/ReusableComponents/NavBar.jsx
+++ client/src/components/ReusableComponents/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react"
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom" // Import useNavigate
 import UserAuth from "../SignIn/SignIn"
 import { AuthContext } from "../Auth/AuthProvider"
 import "../SignIn/signin.css"
@@ -8,12 +8,19 @@ function NavBar() {
   const { auth, logout } = useContext(AuthContext)
   const [showModal, setShowModal] = useState(false)
   const [modalType, setModalType] = useState(null)
-  const location = useLocation() // This hook gives us the current URL location
+  const location = useLocation()
+  const navigate = useNavigate() // Instantiate useNavigate
   const onProfilePage = location.pathname === "/profile"
   const onHomePage = location.pathname === "/"
+
   const openModal = (type) => {
     setModalType(type)
     setShowModal(true)
+  }
+
+  const handleLogout = () => {
+    logout() // Call the logout function from AuthContext
+    navigate("/") // Navigate to home page after logging out
   }
 
   return (
@@ -23,16 +30,24 @@ function NavBar() {
           <i className="fa-solid fa-house text-3xl"></i>
         </Link>
       </div>
-      <div className="container mx-auto flex justify-around text-3xl">
+      <div className="mx-auto flex gap-12 justify-around text-3xl">
         {!auth ? (
-          <>
-            <button onClick={() => openModal("signin")}>
-              <i className="fa-solid fa-right-to-bracket text-3xl"> Sign In</i>
-            </button>
-            <button onClick={() => openModal("signup")}>
-              <i className="fas fa-user-plus text-3xl"> Sign Up</i>
-            </button>
-          </>
+          !showModal && (
+            <>
+              <button onClick={() => openModal("signin")}>
+                <i className="fa-solid fa-right-to-bracket text-3xl">
+                  {" "}
+                  <span> SIGNIN</span>
+                </i>
+              </button>
+              <button onClick={() => openModal("signup")}>
+                <i className="fas fa-user-plus text-3xl">
+                  {" "}
+                  <span>SIGNOUT</span>
+                </i>
+              </button>
+            </>
+          )
         ) : (
           <ul className="link flex flex-row gap-12">
             {onProfilePage && (
@@ -50,20 +65,22 @@ function NavBar() {
               </li>
             )}
             <li>
-              <button onClick={logout}>
+              <button onClick={handleLogout}>
                 <i className="fa-solid fa-arrow-right-from-bracket"> Logout</i>
               </button>
             </li>
           </ul>
         )}
       </div>
-      <UserAuth
-        showModal={showModal}
-        setShowModal={setShowModal}
-        modalType={modalType}
-        setModalType={setModalType}
-        onLogin={() => setShowModal(false)} // Close modal on login
-      />
+      {showModal && (
+        <UserAuth
+          showModal={showModal}
+          setShowModal={setShowModal}
+          modalType={modalType}
+          setModalType={setModalType}
+          onLogin={() => setShowModal(false)} // Close modal on login
+        />
+      )}
     </nav>
   )
 }
