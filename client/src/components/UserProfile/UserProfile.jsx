e022e6db1d261eb0c3aff32bd77c18eed7506a2c
xrmaalk <xinra.inc@gmail.com>
2024-04-21 09:25:31 -0600

    style profile

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -63,58 +63,50 @@ function UserProfile() {
 
       <div className="text-base font-semibold leading-7 justify-center text-green-300 ">
         <div className="px-4 sm:px-0">
-          <h3>{user.username && <span>PROFILE</span>}</h3>
-          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-200">
-            User Information
-          </p>
+          <h1>{user.username && <span>PROFILE</span>}</h1>
         </div>
-        <div className="font-medium text-indigo-300 hover:text-indigo-500 cursor-pointer">
-          <div>Username: {user.username}</div>
-          <div>Email: {user.email}</div>
-        </div>
-        <div className="mt-6 border-t border-gray-100">
-          <dl className="divide-y divide-gray-100">
-            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm font-medium leading-6 text-gray-900">
-                Full name :
-              </dt>
-              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                <span>{user.firstname + " " + user.lastname}</span>
-              </dd>
-            </div>
-            <div className="bg-secondary-gold px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm font-medium leading-6 text-gray-900">
-                Address
-              </dt>
-              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                Place Holder Address
-              </dd>
-            </div>
-            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </dt>
-              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {user.email}
-              </dd>
-            </div>
-            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm font-medium leading-6 text-gray-900">
-                Bio
-              </dt>
-              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                Place Holder Bio
-              </dd>
-            </div>
-            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm font-medium leading-6 text-gray-900">
-                Password
-              </dt>
-              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                Change Password
-              </dd>
-            </div>
-          </dl>
+
+        <div className="mt-6 border-t border-gray-100 rounded-3xl">
+          {/* <dl className="divide-y divide-gray-100"> */}
+          <div className="bg-gray-50 rounded-2xl px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Full name :
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span>{user.firstname + " " + user.lastname}</span>
+            </dd>
+          </div>
+          <div className="bg-secondary-gold px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Address
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Place Holder Address
+            </dd>
+          </div>
+          <div className="rounded-2xl bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {user.email}
+            </dd>
+          </div>
+          <div className="bg-secondary-gold px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Bio</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Place Holder Bio
+            </dd>
+          </div>
+          <div className="bg-gray-50 rounded-2xl px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Password
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Change Password
+            </dd>
+          </div>
+          {/* </dl> */}
         </div>
       </div>
     </div>
