c1adf089394c9d6d6ec01841760ab3088055be56
Shana Adams <shana.adams10@gmail.com>
2024-04-22 08:13:16 -0600

    Add AM/PM selector to AddEvent.

--- client/src/Pages/Calendar/AddEvent.jsx
+++ client/src/Pages/Calendar/AddEvent.jsx
@@ -11,24 +11,34 @@ const addEvent = (hour, event) => {
 function AddEvent({ selectedDate, hour, addEvent }) {
   const [hourInput, setHourInput] = useState("");
   const [eventInput, setEventInput] = useState("");
+  const [amPm, setAmPm] = useState("AM");
   return (
     <div className="addEvent">
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          addEvent(hourInput, eventInput);
+          const hour = parseInt(hourInput);
+          if (isNaN(hour) || hour < 1 || hour > 12) {
+            alert("Hour must be a number between 1 and 12");
+            return;
+          }
+          addEvent(hourInput + ":00 " + amPm, eventInput);
           setHourInput("");
           setEventInput("");
+          setAmPm("AM");
         }}
       >
         <input
-          type="number"
-          min="1"
-          max="24"
+          type="text"
+          pattern="(1[0-2]|0?[1-9]):[0-5][0-9]"
           value={hourInput}
           onChange={(e) => setHourInput(e.target.value)}
           placeholder="Hour"
         />
+        <select value={amPm} onChange={(e) => setAmPm(e.target.value)}>
+          <option value="AM">AM</option>
+          <option value="PM">PM</option>
+        </select>
         <input
           type="text"
           value={eventInput}
